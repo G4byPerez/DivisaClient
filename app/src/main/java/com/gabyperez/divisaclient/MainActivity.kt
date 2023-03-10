badeb4494e33ea0e04028b9a3d6f780eccff5fc2
G4byPerez <78813229+G4byPerez@users.noreply.github.com>
2023-03-09 20:11:27 -0600

    Client al 100

--- app/src/main/java/com/gabyperez/divisaclient/MainActivity.kt
+++ app/src/main/java/com/gabyperez/divisaclient/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.SimpleCursorAdapter
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
@@ -19,7 +20,7 @@ class MainActivity : AppCompatActivity() {
             return CursorLoader(
                 applicationContext,
                 Uri.parse("content://com.gabyperez.divisaserver/monedas"),
-                arrayOf("_ID", "code","value"),
+                arrayOf("_ID","base_code","update","code","value"),
                 null, null, null)
         }
 
@@ -35,37 +36,90 @@ class MainActivity : AppCompatActivity() {
                 val adapter = SimpleCursorAdapter(applicationContext,
                     android.R.layout.simple_list_item_2,
                     this,
-                    arrayOf("_ID", "code","value"),
-                    IntArray(3).apply {
-                        set(1, android.R.id.text1)
-                        set(2, android.R.id.text2)
+                    arrayOf("_ID","base_code","update","code","value"),
+                    IntArray(4).apply {
+                        set(3, android.R.id.text1)
                     } ,
                     SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE
                 )
                 spn.adapter = adapter
+
+                //Show general information
+                var items = adapter.count
+                var info = ""
+                items --
+                var uno = false
+
+
+                for(i in 0..items) {
+                    var cursor = adapter.getItem(i) as Cursor
+                    //Log.d("thisI", i.toString() + " " + cursor.getString(cursor.getColumnIndexOrThrow("code")))
+
+                    if (cursor.getString(cursor.getColumnIndexOrThrow("code")) == "USD") {
+                        if(uno) info += "\n\n"
+                        uno = true
+                        val cursor = adapter.getItem(i) as Cursor
+                        info += "Última actualización: " +
+                                cursor.getString(cursor.getColumnIndexOrThrow("update"))
+
+                        info += "\n"
+
+                        info += cursor.getString(cursor.getColumnIndexOrThrow("code")) + " \t " +
+                                cursor.getString(cursor.getColumnIndexOrThrow("value")) + "\n"
+                    } else {
+                        info += cursor.getString(cursor.getColumnIndexOrThrow("code")) + " \t " +
+                                cursor.getString(cursor.getColumnIndexOrThrow("value")) + "\n"
+                    }
+                }
+                txtJson.append(info)
+
+
+
+                //Click on spinner
+                /*spn.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                        val cursor = adapter.getItem(position) as Cursor
+                        val code = cursor.getString(cursor.getColumnIndexOrThrow("code"))
+                        //txt.text = code
+
+                        //val uri = Uri.parse("content://com.gabyperez.divisaserver/monedas/"+code)
+                        //contentResolver.query(uri, )
+
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>) {
+
+                    }
+                }*/
+
                 while (moveToNext()){
                     Log.i("MONEDAXClienteLC", " id: ${getInt(0)} , code: ${getString(1)}, moneda ${getString(2)}")
                 }
             }
+
+
 
         }
 
     }
 
     lateinit var spn : Spinner
+    lateinit var txtJson : TextView
+
     @SuppressLint("Recycle")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         spn = findViewById(R.id.spinner)
+        txtJson = findViewById(R.id.jsonText)
 
         LoaderManager.getInstance(this)
             .initLoader(1001, null, myLoaderCallbacks)
 
         val micursor   = contentResolver.query(
             Uri.parse("content://com.gabyperez.divisaserver/monedas"),
-            arrayOf("_ID", "code","value"),
+            arrayOf("_ID","base_code","update","code","value"),
             null, null,null
         )
         micursor?.apply {
@@ -73,7 +127,6 @@ class MainActivity : AppCompatActivity() {
                 Log.i("MONEDAXCliente", " id: ${getInt(0)} , code: ${getString(1)}, moneda ${getString(2)}")
             }
         }
-
 
     }
 }
